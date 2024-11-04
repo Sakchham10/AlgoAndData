be406d608b8e2be0c3bf1858941b16da5f0a1f25
Sakchham10 <Sakchham10@Hotmail.com>
2024-11-04 12:25:38 -0600

    Added heapsort

---
+++ assignment_4/heapsort.py
@@ -0,0 +1,32 @@
+def heapify(arr, n, i):
+    largest = i  # Initialize largest as root
+    left = 2 * i + 1  # left = 2*i + 1
+    right = 2 * i + 2  # right = 2*i + 2
+
+    # Check if left child exists and is greater than root
+    if left < n and arr[i] < arr[left]:
+        largest = left
+
+    # Check if right child exists and is greater than largest so far
+    if right < n and arr[largest] < arr[right]:
+        largest = right
+
+    # Change root if needed
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]  # Swap
+
+        # Heapify the root.
+        heapify(arr, n, largest)
+
+
+def heapsort(arr):
+    n = len(arr)
+
+    # Build a max-heap
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    # Extract elements from heap one by one
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]  # Swap
+        heapify(arr, i, 0)
